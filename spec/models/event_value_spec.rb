6f36a5a637858ccdab98b5db7fbadbdfe5f334ea
Danilo <danilo.ribeirodr01@gmail.com>
2024-04-24 01:18:51 -0300

    Ajustado testes do event value. Adicionado condição de obrigação do valor extra

--- spec/models/event_value_spec.rb
+++ spec/models/event_value_spec.rb
@@ -3,19 +3,25 @@ require 'rails_helper'
 RSpec.describe EventValue, type: :model do
   describe "#valid" do 
     it "Cadastro correto" do 
-      event_value = EventValue.new(base_price: 50.39, price_per_person: 30)
+      event_value = EventValue.new(base_price: 50.39, price_per_person: 30, overtime_rate: 50)
       
       expect(event_value.valid?).to eq true
     end
 
     it "Preço base obrigatório" do 
-      event_value = EventValue.new(price_per_person: 30)
+      event_value = EventValue.new(price_per_person: 30, overtime_rate: 50)
       
       expect(event_value.valid?).to eq false
     end
 
     it "Preço por pessoa obrigatório" do 
-      event_value = EventValue.new(base_price: 50.39)
+      event_value = EventValue.new(base_price: 50.39, overtime_rate: 50)
+      
+      expect(event_value.valid?).to eq false
+    end
+
+    it "Valor da hora extra obrigatório" do 
+      event_value = EventValue.new(base_price: 50.39, price_per_person: 30)
       
       expect(event_value.valid?).to eq false
     end
