45e6314017183012b55adc174f45af3cb7bf6b6d
Danilo <danilo.ribeirodr01@gmail.com>
2024-04-26 12:02:00 -0300

    Lista buffets cadastrados na tela inicial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
     if user_signed_in? && current_user.company
       @buffet_registration = BuffetRegistration.find_by(user: current_user.id)
       @event_types =  EventType.where("user_id = #{current_user.id}")
+      return
     end
+
+    @buffet_records = BuffetRegistration.all
   end
 end
