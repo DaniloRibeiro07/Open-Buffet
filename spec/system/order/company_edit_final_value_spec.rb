9cccb7a090d67b198ab70aabf5a0e3f0c4550f30
Danilo <danilo.ribeirodr01@gmail.com>
2024-05-06 04:34:49 -0300

    Adicionado testes de adição e edição do valor final e atualizado testes ja existentes

--- spec/system/order/company_edit_final_value_spec.rb
+++ spec/system/order/company_edit_final_value_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'Dono do buffet acessa a pagína de visualização do buffet que espera a sua avaliação' do 
-  it 'e cadastra o valor final com sucesso com justificativa' do 
+describe 'Dono do buffet acessa a pagína de visualização do buffet que espera a avaliação do client' do 
+  it 'e edita o valor final com sucesso' do 
     user = User.create!(name: "Alecrim", last_name: "Farias", email: 'Alecrim@teste.com', password: 'teste123', company: true)
     
     payment_method = PaymentMethod.create!(pix: true, boleto:true, bitcoin: true)
@@ -23,16 +23,16 @@ describe 'Dono do buffet acessa a pagína de visualização do buffet que espera
 
     extra_service = ExtraService.new(decoration: true)
     order = Order.create!(user: cliente, event_type: event, buffet_registration: buffet_registration, date: 1.day.from_now, 
-                          amount_of_people: 54, duration: 35, inside_the_buffet: true, extra_service: extra_service)
-
+                          amount_of_people: 54, duration: 35, inside_the_buffet: true, extra_service: extra_service,
+                    final_value: 55, justification_final_value: "Imposto", expiration_date: 1.day.from_now, payment_method: "pix")
+    order.waiting_for_client_review!
     login_as user
     visit root_path
     click_on "Pedidos"
     click_on order.code
 
+    click_on "Editar Valor Final"
     fill_in "Valor Final",	with: "900" 
-    fill_in "Motivo do Valor Final",	with: "Imposto e taxa do site"
-    fill_in "Validade do Pedido",	with: Date.current
     choose "Boleto"
     click_on "Cadastrar Valor Final"
 
@@ -40,7 +40,7 @@ describe 'Dono do buffet acessa a pagína de visualização do buffet que espera
     expect(page).to have_content "Status do Pedido: Aguardando a Análise do Cliente" 
   end
 
-  it 'e cadastra o valor final com sucesso sem justificativa' do 
+  it 'e edita o valor final com sucesso sem justificativa' do 
     user = User.create!(name: "Alecrim", last_name: "Farias", email: 'Alecrim@teste.com', password: 'teste123', company: true)
     
     payment_method = PaymentMethod.create!(pix: true, boleto:true, bitcoin: true)
@@ -62,24 +62,28 @@ describe 'Dono do buffet acessa a pagína de visualização do buffet que espera
 
     extra_service = ExtraService.new(decoration: true)
     order = Order.create!(user: cliente, event_type: event, buffet_registration: buffet_registration, date: 1.day.from_now, 
-                          amount_of_people: 54, duration: 35, inside_the_buffet: true, extra_service: extra_service)
+                          amount_of_people: 54, duration: 35, inside_the_buffet: true, extra_service: extra_service,
+            final_value: 55, justification_final_value: "Imposto", expiration_date: 1.day.from_now, payment_method: "pix")
+    order.waiting_for_client_review!
 
     login_as user
     visit root_path
     click_on "Pedidos"
     click_on order.code
 
+    click_on "Editar Valor Final"
     fill_in "Valor Final",	with: "1370" 
     fill_in "Motivo do Valor Final",	with: ""
     fill_in "Validade do Pedido",	with: Date.current
-    choose "PIX"
+    choose "Boleto"
+
     click_on "Cadastrar Valor Final"
 
     expect(page).to have_content "Adicionado valor final com sucesso"
     expect(page).to have_content "Status do Pedido: Aguardando a Análise do Cliente" 
   end
 
-  it 'deixa todos os campos vázios' do 
+  it 'deixa campos obrigatórios vazios' do 
     user = User.create!(name: "Alecrim", last_name: "Farias", email: 'Alecrim@teste.com', password: 'teste123', company: true)
     
     payment_method = PaymentMethod.create!(pix: true, boleto:true, bitcoin: true)
@@ -101,54 +105,21 @@ describe 'Dono do buffet acessa a pagína de visualização do buffet que espera
 
     extra_service = ExtraService.new(decoration: true)
     order = Order.create!(user: cliente, event_type: event, buffet_registration: buffet_registration, date: 1.day.from_now, 
-                          amount_of_people: 54, duration: 35, inside_the_buffet: true, extra_service: extra_service)
+                          amount_of_people: 54, duration: 35, inside_the_buffet: true, extra_service: extra_service,
+            final_value: 55, justification_final_value: "Imposto", expiration_date: 1.day.from_now, payment_method: "pix")
+    order.waiting_for_client_review!
 
     login_as user
     visit root_path
     click_on "Pedidos"
     click_on order.code
 
+    click_on "Editar Valor Final"
+    fill_in "Valor Final",	with: "" 
+    fill_in "Motivo do Valor Final",	with: ""
     click_on "Cadastrar Valor Final"
 
     expect(page).to have_content "Há 1 erros encontrados:"
     expect(page).to have_content "Valor Final obrigatório" 
-  end
-
-  it 'deixa todos os campos, exceto valor final vázios' do 
-    user = User.create!(name: "Alecrim", last_name: "Farias", email: 'Alecrim@teste.com', password: 'teste123', company: true)
-    
-    payment_method = PaymentMethod.create!(pix: true, boleto:true, bitcoin: true)
-
-    buffet_registration = BuffetRegistration.create!(user: user, payment_method: payment_method, trading_name: 'Buffet da familia', company_name: 'Eduarda Buffet', 
-      cnpj: "17924491000160", phone: "7995876812", email: 'Maria@teste.com', public_place: "Quadra 1112 Sul Alameda 5", address_number: "25A", 
-      neighborhood: "Plano Diretor Sul", state: "TO", city: "Palmas", zip: "77024-171", complement: "", description: "O melhor buffet das perfumaras")
-
-    event_value = EventValue.create!(base_price: 50, price_per_person: 30, overtime_rate: 30)
-
-    event = EventType.create!(different_weekend: true , weekend_price: event_value, working_day_price: event_value,
-      buffet_registration: buffet_registration, name: "Chá de revelação", description: "Chá de revelação para novos pais ",
-      minimum_quantity: 10, maximum_quantity: 55, duration: 63, menu: "Bolo, salgados e docinhos", 
-      alcoholic_beverages: true, decoration: true, valet: true, insider: true, outsider: false, user: user)
-
-    cliente = User.new(name: "Sabrina", last_name: "Juan", email: 'Sabrina@teste.com', password: 'teste123', company: false)
-    cliente.build_client_datum(cpf: "97498970058")
-    cliente.save!
-
-    extra_service = ExtraService.new(decoration: true)
-    order = Order.create!(user: cliente, event_type: event, buffet_registration: buffet_registration, date: 1.day.from_now, 
-                          amount_of_people: 54, duration: 35, inside_the_buffet: true, extra_service: extra_service)
-
-    login_as user
-    visit root_path
-    click_on "Pedidos"
-    click_on order.code
-
-    fill_in "Valor Final",	with: "500" 
-    click_on "Cadastrar Valor Final"
-
-    expect(page).to have_content "Há 3 erros encontrados:"
-    expect(page).to have_content "Motivo do Valor Final precisa ser informada caso o valor seja diferente do calculado"
-    expect(page).to have_content "Validade do Pedido deve ser menor ou igual a data do evento e maior ou igual a data de hoje"
-    expect(page).to have_content "Forma de pagamento deve ser especificado" 
   end
 end
--- spec/system/order/user_edit_order_spec.rb
+++ spec/system/order/user_edit_order_spec.rb
@@ -592,4 +592,111 @@ describe "Cliente Autenticado clica em Editar Pedido" do
       expect(page).not_to have_content "Pedido Atualizado com sucesso"
     end
   end
+
+  context "Pedidos Aguardando o cliente" do 
+    it 'Com sucesso' do 
+      user = User.create!(name: "Alecrim", last_name: "Farias", email: 'Alecrim@teste.com', password: 'teste123', company: true)
+    
+      payment_method = PaymentMethod.create!(pix: true)
+  
+      buffet_registration = BuffetRegistration.create!(user: user, payment_method: payment_method, trading_name: 'Buffet da familia', company_name: 'Eduarda Buffet', 
+        cnpj: "17924491000160", phone: "7995876812", email: 'Maria@teste.com', public_place: "Quadra 1112 Sul Alameda 5", address_number: "25A", 
+        neighborhood: "Plano Diretor Sul", state: "TO", city: "Palmas", zip: "77024-171", complement: "", description: "O melhor buffet das perfumaras")
+  
+      event_value = EventValue.create!(base_price: 50.39, price_per_person: 30, overtime_rate: 30)
+  
+      event = EventType.create!(different_weekend: true , weekend_price: event_value, working_day_price: event_value,
+        buffet_registration: buffet_registration, name: "Chá de revelação", description: "Chá de revelação para novos pais ",
+        minimum_quantity: 10, maximum_quantity: 55, duration: 63, menu: "Bolo, salgados e docinhos", 
+        alcoholic_beverages: true, decoration: true, valet: true, insider: true, outsider: false, user: user)
+  
+      cliente = User.new(name: "Sabrina", last_name: "Juan", email: 'Sabrina@teste.com', password: 'teste123', company: false)
+      cliente.build_client_datum(cpf: "97498970058")
+      cliente.save!
+  
+      allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('GZHPQ5GO')
+      extra_service = ExtraService.new(decoration: true)
+      order = Order.create!(user: cliente, event_type: event, buffet_registration: buffet_registration, date: 1.day.from_now, 
+                            amount_of_people: 54, duration: 35, inside_the_buffet: true, extra_service: extra_service,
+              final_value: 55, justification_final_value: "Imposto", expiration_date: 1.day.from_now, payment_method: "pix")
+      order.waiting_for_client_review!
+                
+      
+      login_as cliente
+      visit root_path
+      click_on "Meus pedidos"
+      click_on "GZHPQ5GO"
+      click_on "Editar Pedido"
+
+      fill_in "Participantes do Evento",	with: "50" 
+      fill_in "Duração do Evento",	with: "80" 
+      check "Bebidas Alcoólicas"
+      check "Serviço de Valete/Estacionamento"
+      uncheck "Decorações"
+
+      click_on "Submeter"
+
+      expect(current_path).to eq order_path(order)
+      expect(page).to have_content "Pedido Atualizado com sucesso"
+      expect(page).to have_content "Status do Pedido: Aguardando Análise do Buffet"
+
+    end
+
+    it 'Com falha' do 
+      user = User.create!(name: "Alecrim", last_name: "Farias", email: 'Alecrim@teste.com', password: 'teste123', company: true)
+      
+      payment_method = PaymentMethod.create!(pix: true)
+
+      buffet_registration = BuffetRegistration.create!(user: user, payment_method: payment_method, trading_name: 'Buffet da familia', company_name: 'Eduarda Buffet', 
+        cnpj: "17924491000160", phone: "7995876812", email: 'Maria@teste.com', public_place: "Quadra 1112 Sul Alameda 5", address_number: "25A", 
+        neighborhood: "Plano Diretor Sul", state: "TO", city: "Palmas", zip: "77024-171", complement: "", description: "O melhor buffet das perfumaras")
+
+      event_value = EventValue.create!(base_price: 50.39, price_per_person: 30, overtime_rate: 30)
+
+      event = EventType.create!(different_weekend: true , weekend_price: event_value, working_day_price: event_value,
+        buffet_registration: buffet_registration, name: "Chá de revelação", description: "Chá de revelação para novos pais ",
+        minimum_quantity: 10, maximum_quantity: 55, duration: 63, menu: "Bolo, salgados e docinhos", 
+        alcoholic_beverages: true, decoration: true, valet: true, insider: false, outsider: true, user: user)
+
+      cliente = User.new(name: "Sabrina", last_name: "Juan", email: 'Sabrina@teste.com', password: 'teste123', company: false)
+      cliente.build_client_datum(cpf: "97498970058")
+      cliente.save!
+
+      allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('GZHPQ5GO')
+      extra_service = ExtraService.new(decoration: true)
+      customer_address = CustomerAddress.create!(public_place: "Alameda Chile", address_number: "69", 
+                          neighborhood: "Jardim Europa", state: "AC", city: "Rio Branco", zip: "69915485", complement: "")
+      order = Order.create!(user: cliente, event_type: event, buffet_registration: buffet_registration, date: 1.day.from_now, 
+              amount_of_people: 54, duration: 35, inside_the_buffet: false, extra_service: extra_service, customer_address: customer_address,
+              final_value: 55, justification_final_value: "Imposto", expiration_date: 1.day.from_now, payment_method: "pix")
+
+      order.waiting_for_client_review!
+      
+      login_as cliente
+      visit root_path
+      click_on "Meus pedidos"
+      click_on "GZHPQ5GO"
+      click_on "Editar Pedido"
+
+      fill_in "Participantes do Evento",	with: "24" 
+      fill_in "Duração do Evento",	with: "68" 
+      fill_in "Logradouro",	with: "Rua Sao Maritano" 
+      fill_in "N°",	with: "15" 
+      fill_in "Bairro",	with: "" 
+      fill_in "Estado",	with: "Sergipe" 
+      fill_in "Cidade",	with: "Arara" 
+      fill_in "CEP",	with: ""
+      fill_in "Complemento",	with: "Próximo ao super mercado" 
+      check "Bebidas Alcoólicas"
+
+      click_on "Submeter"
+
+      expect(current_path).not_to eq order_path(order)
+      expect(page).to have_content "Há 2 erros encontrados:" 
+      expect(page).to have_content "Bairro não pode ficar em branco" 
+      expect(page).to have_content "CEP não pode ficar em branco" 
+      expect(page).not_to have_content "Pedido Atualizado com sucesso"
+    end
+
+  end
 end
