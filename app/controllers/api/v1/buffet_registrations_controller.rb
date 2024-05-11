e49dd292c7db1c2f35aa707cc4f3e2f20e4f1aa0
Danilo <danilo.ribeirodr01@gmail.com>
2024-05-11 18:01:42 -0300

    Adicionado a API - Listagem de buffets concluida

--- app/controllers/api/v1/buffet_registrations_controller.rb
+++ app/controllers/api/v1/buffet_registrations_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::BuffetRegistrationsController < ActionController::API
   def index 
-    render status:200, json: BuffetRegistration.all.map(&:trading_name)
+    filter = params[:filter]
+    if filter
+      result = BuffetRegistration.where('trading_name LIKE ?', "%#{filter}%")
+      if result.any? 
+        render status: 200, json: result.map(&:trading_name)
+      else
+        render status: 406, json: {"errors": "Não foi encontrados registros de #{filter}"}
+      end
+    else 
+      render status:200, json: BuffetRegistration.all.map(&:trading_name)
+    end
   end
 end
